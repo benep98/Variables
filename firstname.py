6af98af0ec4d7d354a9a0f8458f903dc5e98aad0
Benjamin Pope <40139@longroad.ac.uk>
2014-09-09 15:12:42 +0100

    firstname python

---
+++ firstname.py
@@ -0,0 +1,8 @@
+print ("Hello world")
+
+first_name = input ("Please enter your name")
+#= means assign, input means input from keyboard and first_name is the identifier
+print (first_name)
+#Prints the name entered
+print ("Hi {0} !".format(first_name))
+#{0} Means space allocated for the name to be printed
