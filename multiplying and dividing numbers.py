fe20f89cca7952d0c81ca7b04d1c2ec3f1b7475d
Benjamin Pope <40139@longroad.ac.uk>
2014-09-22 12:18:59 +0100

    workkk

--- multiplying and dividing numbers.py
+++ multiplying and dividing numbers.py
@@ -5,3 +5,4 @@ third_number = int(input("Please enter your third number"))
 answer1 = first_number * second_number
 answer2 = answer1 / third_number
 print(answer2)
+print("{0:.0f}".format(answer2))
